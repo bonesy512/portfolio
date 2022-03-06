164d2fb7033f595fa48cd8e609bcebe60744bd05
Thomas Schustereit <bonesy@bonesydesign.com>
2022-03-05 19:59:21 -0600

    fingers crossed

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-5QPMT126HB"></script>
+    {/* Site Tag */}
+    <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],e.factory=function(t){return function(){var r=Array.prototype.slice.call(arguments);return r.unshift(t),e.push(r),e}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.rudderlabs.com/v1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
+              e.load("25zZZ4EGpiz5tTONvLGPBKZ8PPY","https://bonesydesixvax.dataplane.rudderstack.com"),
+              e.ready(() => {
+                  console.log("We are set!!!")
+              })
+          }();
+          `
+            }}
+          />
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
         <meta property="og:site_name" content={meta.title} />
